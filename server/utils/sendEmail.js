8ed033e23ce6ef9d24a2cccd9cd9bdba3c0bbf50
Alana Prem <alanaprem001@gmail.com>
2026-06-14 11:45:54 +0530

    Update the email config

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,4 +1,8 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+// Force IPv4 resolution for Node.js 17+ as Render does not support outbound IPv6
+dns.setDefaultResultOrder("ipv4first");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
