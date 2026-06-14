60bb5aad4249c1b35e7625140879cd407ff6e6bf
Alana Prem <alanaprem001@gmail.com>
2026-06-14 11:22:47 +0530

    Update

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,6 +4,7 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    family: 4,
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
